bc4cba54ea15972aba894f1c39f507bbf56ee0bf
Emir Bolat <emirybolat@hotmail.com>
2025-05-27 01:50:05 +0300

    Refactor BaseViewModel to provide a default implementation for init method

--- speedev_ui/lib/src/core/base/base_view_model.dart
+++ speedev_ui/lib/src/core/base/base_view_model.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
 abstract class BaseViewModel extends ChangeNotifier {
-  void init();
-
-  @override
-  void dispose();
+  void init() {}
 
   void updateState() => notifyListeners();
 }
